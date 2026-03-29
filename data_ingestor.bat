f0280c8b927b8807cb457bc0c5db12daf28b2567
Chih-Yueh Huang <38789142+cyhuangg@users.noreply.github.com>
2026-03-29 00:02:46 +0000

    data ingestion from local

---
+++ data_ingestor.bat
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DATABASE_URL=postgresql://db_68e9_user:k5M4KoYVM5iam92Il5FERVRpbOs57N5w@dpg-d73pid9r0fns73clp24g-a.frankfurt-postgres.render.com/db_68e9
+echo Starting ingestion to Render...
+py src/ingest.py
+echo Ingestion complete!
+pause
